573203fa28ad4f138d324c7662633ae1bfbe605d
hoang <hoang0606hh@gmail.com>
2025-04-09 23:15:45 +0700

    Thêm lại route /hello

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/hello', function () {
     return response()->json([
         'message' => 'Hello chào mừng đến Đề tài Thi trắc Nghiệm'
     ]);
