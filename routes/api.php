c787bfd7acdee70c57591cae5d1e6b52d2792043
hoang <hoang0606hh@gmail.com>
2025-04-09 23:13:26 +0700

    Xoá route hello

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
+Route::get('/', function () {
     return response()->json([
         'message' => 'Hello chào mừng đến Đề tài Thi trắc Nghiệm'
     ]);
