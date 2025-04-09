92a7fe43ae26a1d8e3d3ac77900450770bd9d646
hoang <hoang0606hh@gmail.com>
2025-04-09 21:51:30 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Hello chào mừng đến Đề tài Thi trắc Nghiệm'
+    ]);
+});
+
